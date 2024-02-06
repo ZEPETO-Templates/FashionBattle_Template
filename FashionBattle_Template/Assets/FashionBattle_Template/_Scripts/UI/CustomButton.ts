7a32e873a449493e4738b6bb704febf7f7e9103b
Sol <soledad.coronel@captivatar.com>
2024-02-06 12:29:11 -0300

    Btns Selection States  and Removed prefab

--- FashionBattle_Template/Assets/FashionBattle_Template/_Scripts/UI/CustomButton.ts
+++ FashionBattle_Template/Assets/FashionBattle_Template/_Scripts/UI/CustomButton.ts
@@ -1,32 +1,23 @@
-import { Color, Sprite } from 'UnityEngine';
-import { Image } from 'UnityEngine.UI'
+import { GameObject } from 'UnityEngine';
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 
 export default class CustomButton extends ZepetoScriptBehaviour {
 
-    @SerializeField() private iconRef: Image; // Reference to the icon
-    @SerializeField() private defaultColor: Color; // Reference to the default color
-    @SerializeField() private highlightColor: Color; // Reference to the hightlight color
+    @SerializeField() private iconRef: GameObject; // Reference to the icon
+    public selectedImage: GameObject; // Reference to the selected image
+   
 
-    // This method is responsible to set this button and saves the icon ref
-    public SetCustomButton(iconSprite: Sprite)
-    {
-        // Saves the icon ref
-        this.iconRef.sprite = iconSprite;
-    }
-
-    // This method is called on click button, is responsible to change hightlight
+    // This method is called on click button, is responsible to change image
     public SelectButton()
     {
-        // Set the main color to hightlight color
-        this.iconRef.color = this.highlightColor;
+        this.selectedImage.SetActive(true);
     }
 
     // This method is responsible to reset button color
     public ResetButton()
     {
-        // Set the main color to default
-        this.iconRef.color = this.defaultColor;
+        this.iconRef.SetActive(true);
+        this.selectedImage.SetActive(false);
     }
 
 }
